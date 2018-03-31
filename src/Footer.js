28fdad1ffc9287d5a35e52ce69a47828f294fe9c
Sergey Sergienko <sergse2012@yandex.ru>
2018-03-31 15:25:37 +0300

    App structure refactoring commit

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-	<footer>
-		<p>Copyright © 2018. All rights reserved. Developed by Crazy Nails Studio Production.</p>
-	</footer>
-)
-
-export default Footer;
